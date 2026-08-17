c99b871ae03d8885a6446084d9efb5e3a5e6dbef
Wtvitin <victor.samuel1609@gmail.com>
2026-08-17 20:15:40 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,10 @@
+print("Resolvendo um problema de atividade...")
+
+x = int(input("Digite um número inteiro: "))
+
+dobro = x * 2
+resultado = x + dobro
+
+print(f"O número digitado é{x}")
+print(f"O dobro de {x} é {dobro}")
+print(f"O resultado de {x} + {dobro} é {resultado}")
